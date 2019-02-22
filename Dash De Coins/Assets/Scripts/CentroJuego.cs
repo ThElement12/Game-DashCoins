80088b4cec403aea3e1a65c683b9d7b5c5782ce0
ThElemenet12 <45013990+ThElemenet12@users.noreply.github.com>
2019-02-22 09:38:10 -0400

    Cambios y scripts

---
+++ Dash De Coins/Assets/Scripts/CentroJuego.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CentroJuego : MonoBehaviour
+{
+
+    public enum EstadoJuego
+    {
+        Fase1,
+        Fase2
+    };
+    public static EstadoJuego estado;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        estado = EstadoJuego.Fase1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Dash De Coins/Assets/Scripts/GenerarBloques.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerarBloques : MonoBehaviour
+{
+    public GameObject[] bloques = new GameObject[2];
+    GameObject plataforma;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Gen());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator Gen()
+    {
+        while (true)
+        {
+            if(CentroJuego.estado == CentroJuego.EstadoJuego.Fase1)
+            {
+                plataforma = Instantiate(bloques[0], new Vector3(gameObject.transform.position.x + 2, gameObject.transform.position.y), Quaternion.identity);
+                plataforma.GetComponent<Rigidbody2D>().AddForce(new Vector2(-10, 0), ForceMode2D.Impulse);
+            }
+
+
+
+           // yield return new WaitForSeconds(5);
+
+        }
+    }
+    
+}
