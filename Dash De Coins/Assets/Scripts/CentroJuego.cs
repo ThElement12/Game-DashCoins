c4927b412fef14e7ef5d01cec9da34646924af85
KillerCode_236 <45013989+Rubencrash236@users.noreply.github.com>
2019-02-23 20:17:31 -0400

    iihvh

--- Dash De Coins/Assets/Scripts/CentroJuego.cs
+++ Dash De Coins/Assets/Scripts/CentroJuego.cs
@@ -11,7 +11,9 @@ public class CentroJuego : MonoBehaviour
         Fase2
     };
     public static EstadoJuego estado;
-    
+    public GameObject[] bloques = new GameObject[2];
+    GameObject plataforma;
+    int count = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +23,26 @@ public class CentroJuego : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        switch (estado)
+        {
+            case EstadoJuego.Fase1:
+                if (count == 0)
+                {
+                    count = 20;
+                    plataforma = Instantiate(bloques[0], new Vector3(14.24619f, -3.805211f), Quaternion.identity);
+                    //plataforma.transform.Translate();
+                    plataforma.GetComponent<Rigidbody>().AddForce(new Vector3(-10, 0), ForceMode.Impulse);
+                }
+                else
+                {
+                    count--;
+                }
+                break;
+
+            case EstadoJuego.Fase2:
+                break;
+        }
+
         
     }
 }
--- Dash De Coins/Assets/Scripts/GenerarBloques.cs
+++ Dash De Coins/Assets/Scripts/GenerarBloques.cs
@@ -7,25 +7,48 @@ public class GenerarBloques : MonoBehaviour
 {
     public GameObject[] bloques = new GameObject[2];
     GameObject plataforma;
+    int count = 20;
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(Gen());
+        
+        //StartCoroutine(Gen());
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        
+        if (CentroJuego.estado == CentroJuego.EstadoJuego.Fase1)
+        {
+            if (count == 0)
+            {
+                count = 20;
+                plataforma = Instantiate(bloques[0], new Vector3(gameObject.transform.position.x + 2, gameObject.transform.position.y), Quaternion.identity);
+                plataforma.GetComponent<Rigidbody2D>().AddForce(new Vector2(-10, 0), ForceMode2D.Impulse);
+            }
+            else
+            {
+                count--;
+            }
+        }
     }
     IEnumerator Gen()
     {
         while (true)
         {
-            if(CentroJuego.estado == CentroJuego.EstadoJuego.Fase1)
+            if (CentroJuego.estado == CentroJuego.EstadoJuego.Fase1)
             {
-                plataforma = Instantiate(bloques[0], new Vector3(gameObject.transform.position.x + 2, gameObject.transform.position.y), Quaternion.identity);
-                plataforma.GetComponent<Rigidbody2D>().AddForce(new Vector2(-10, 0), ForceMode2D.Impulse);
+                if (count == 0)
+                {
+                    count = 20;
+                    plataforma = Instantiate(bloques[0], new Vector3(gameObject.transform.position.x + 2, gameObject.transform.position.y), Quaternion.identity);
+                    plataforma.GetComponent<Rigidbody2D>().AddForce(new Vector2(-10, 0), ForceMode2D.Impulse);
+                }
+                else
+                {
+                    count--;
+                }
             }
 
 
