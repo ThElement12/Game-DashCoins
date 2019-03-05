08fb651dca449cd4ddeeb6e1b83fcacfd237c9ee
KillerCode_236 <45013989+Rubencrash236@users.noreply.github.com>
2019-03-04 22:06:10 -0400

    asdkl

---
+++ Dash De Coins/Assets/Scripts/ControlMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnMouseEnter()
+    {
+        gameObject.transform.localScale *= 1.2f;
+    }
+
+    private void OnMouseExit()
+    {
+        gameObject.transform.localScale /= 1.2f;
+    }
+
+    public void OnMouseUp()
+    {
+        switch (gameObject.name)
+        {
+            case "Quit":
+                Application.Quit();
+                break;
+            case "Play":
+                SceneManager.LoadScene("Dash de coins");
+
+                break;
+            //case "Options":
+               // GameObject.Find("New Text").GetComponent<MenuControl2>().ShowCanvas();
+                //break;
+        }
+    }
+}
